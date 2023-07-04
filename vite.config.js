caada5f81965dc760b5c8a59744d268b095f8332
nextc <1391040917@qq.com>
2023-07-04 21:15:02 +0800

    chore: 优化代理配置

--- vite.config.js
+++ vite.config.js
@@ -1,66 +1,71 @@
 // vite.config.ts
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { viteMockServe } from 'vite-plugin-mock'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-const proxy_target = 'http://localhost:8080'
-const contexts = [
-  '/plc_config',
-  '/run_task',
-  '/plc_configs',
-  '/createDataTable',
-]
-const proxy = {}
-for (let context of contexts) {
-  proxy[context] = {
-    target: proxy_target,
-    changeOrigin: true,
-    pathRewrite: { [`^${context}`]: '' },
-  }
-}
-
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      // 设置路径
-      '~': path.resolve(__dirname, './'),
-      // 设置别名
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: './',
+    resolve: {
+      alias: {
+        // 设置别名
+        '@': path.resolve(__dirname, './src'),
+      },
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-  },
-  esbuild: {
-    // 打包去除console和debugger
-    drop: ['console', 'debugger'],
-  },
-  plugins: [
-    // 打包体积视图分析
-    visualizer({
-      gzipSize: true,
-      brotliSize: true,
-      emitFile: false,
-      filename: 'test.html', //分析图生成的文件名
-      open: true, //如果存在本地服务端口，将在打包后自动展示
-    }),
-    vue(),
-    // element ui自动引入
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  // node服务
-  server: {
-    host: true,
-    open: true,
-    hmr: true, // 热更新
-    proxy, // 配置代理
-  },
+    esbuild: {
+      // 打包去除console和debugger
+      drop: ['console', 'debugger'],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/style/variable.scss";',
+        },
+      },
+    },
+    plugins: [
+      // 打包体积视图分析
+      visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        filename: 'test.html', //分析图生成的文件名
+        open: true, //如果存在本地服务端口，将在打包后自动展示
+      }),
+      vue(),
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+      // element ui自动引入
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    // node服务
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          //获取数据的服务器地址设置
+          target: env.VITE_SERVE,
+          //需要代理跨域
+          changeOrigin: true,
+          //路径重写
+          rewrite: (path) =>
+            path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ''),
+        },
+      },
+    },
+  }
 })
